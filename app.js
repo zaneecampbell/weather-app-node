016571d67f486bebf560fac475e5d603dd1861a6
Zane Campbell <zanegitinit@gmail.com>
2019-02-01 15:30:24 -0800

    formatted JSON

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
     url: 'http://www.mapquestapi.com/geocoding/v1/address?key=wDHtLASd5RVoGHXOKFSiX2ORMRpG7Yzd&location=1301%20lombard%20street%20philadelphia',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2));
 });
