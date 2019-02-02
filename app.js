0c680b6e1efd7e0759f7046d1f8954e73547a8c7
Zane Campbell <zanegitinit@gmail.com>
2019-02-02 15:37:49 -0800

    setup dynamic request through user input

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+    const encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'http://www.mapquestapi.com/geocoding/v1/address?key=wDHtLASd5RVoGHXOKFSiX2ORMRpG7Yzd&location=1301%20lombard%20street%20philadelphia',
+    url: `http://www.mapquestapi.com/geocoding/v1/address?key=wDHtLASd5RVoGHXOKFSiX2ORMRpG7Yzd&location=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].providedLocation.location}`);
-    console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`)
-    console.log(`Longitutde: ${body.results[0].locations[0].latLng.lng}`)
+    console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
+    console.log(`Longitutde: ${body.results[0].locations[0].latLng.lng}`);
 });
