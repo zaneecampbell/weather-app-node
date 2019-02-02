1d6d687154b61314e796c652927f7765b35a16bd
Zane Campbell <zanegitinit@gmail.com>
2019-02-02 15:17:09 -0800

    setup getting lat and lon from JSON

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ request({
     url: 'http://www.mapquestapi.com/geocoding/v1/address?key=wDHtLASd5RVoGHXOKFSiX2ORMRpG7Yzd&location=1301%20lombard%20street%20philadelphia',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].providedLocation.location}`);
+    console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`)
+    console.log(`Longitutde: ${body.results[0].locations[0].latLng.lng}`)
 });
